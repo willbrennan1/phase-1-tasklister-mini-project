916ee8b38214f68cede0bd2052d388396bd9e349
Will Brennan <willbrennan314@gmail.com>
2021-11-10 09:03:44 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let inputValue = document.getElementById("new-task-description").value
+    handleToDo(inputValue);
+    form.reset();
+
+  });
+})
+
+function handleToDo(todo) {
+  let p = document.createElement('p');
+  let bttn = document.createElement('button');
+  bttn.addEventListener('click', handleDelete);
+  bttn.textContent = 'x';
+  p.textContent = `${todo} `;
+  p.appendChild(bttn);
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
+
+
+// As a user, I should be able to type a task into the input field.
+// As a user, I should be able to click some form of a submit button.
+// As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
